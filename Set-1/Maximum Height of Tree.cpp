82dc3f94dcdae062c692f5a851bf878cbcc791c1
Smriti Mishra <hi.smritimishra@gmail.com>
2022-06-30 14:50:04 -0300

    Removing files

--- Set-1/Maximum Height of Tree.cpp
+++
@@ -1,9 +0,0 @@
- //Problem Name: Maximum Height of Tree
- //Source : GeeksForGeeks
- /****************************************/
-int height(struct Node* node){
-        if(node == NULL)
-        return 0;
-        
-        return 1 + max(height(node->left), height(node->right));
-    }
