b6ed51ecf8a8354692ce6069cd35e26134475d44
Smriti Mishra <hi.smritimishra@gmail.com>
2022-06-30 14:46:16 -0300

    String Set 1

---
+++ Set-1/String/Permutations of a given string.cpp
@@ -0,0 +1,19 @@
+	void helper(string S, int id, set<string> &ans){
+	    if(id == S.length()-1){
+	        ans.insert(S);
+	        return;
+	    }
+	    
+	    for(int i = id; i < S.length(); i++){
+	        swap(S[i], S[id]);
+	        helper(S, id+1, ans);
+	        swap(S[i], S[id]); //backtracking
+	    }
+	}
+	
+	vector<string>find_permutation(string S){
+		 set<string> ans;
+		 helper(S, 0, ans);
+		 vector<string>res(ans.begin(), ans.end());
+		 return res;
+	}
