4c82fe39d709ccd74e66acb942e814fcb1b1a4ab
Smriti Mishra <hi.smritimishra@gmail.com>
2022-07-02 19:26:00 -0300

    Theory Set 1

---
+++ Set-1/Theory/Class-Vs-Structure.cpp
@@ -0,0 +1,28 @@
+/* ***********************************************
+       STRUCTURE VS CLASSES in c++
+**************************************************/
+
+struct struct_name{
+    //members
+    //member functions
+}
+
+// Structure:
+// ->It is a user define data type
+// ->cannot modify constructor and has no distructor
+// ->All values are stored in STACK memory
+// ->Member function cannot be abstract or virtual.
+// ->Without access specifier, the member of the structure are public
+
+
+class class-name{
+    //members
+    //memberfunctions
+}
+
+// Classes:
+// ->Class is a blueprint of an object
+// ->Class has constructor and can be modified and it has a distructor.
+// ->All values are stored in the HEAP memory.
+// ->Member function can be abstract or virtual.
+// ->Without access specifier, the member of the structure are private.
