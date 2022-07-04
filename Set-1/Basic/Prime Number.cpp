ca475684eb768902285f21aae02f6f3e17f8703a
Smriti Mishra <hi.smritimishra@gmail.com>
2022-07-03 22:28:46 -0300

    Basic Set 1

---
+++ Set-1/Basic/Prime Number.cpp
@@ -0,0 +1,13 @@
+//Problem Name: Prime Number
+//Source : GeeksForGeeks
+/****************************************/
+  int isPrime(int N){
+        if(N==1)
+            return false;
+       
+        for(int i = 2; i*i <= N; i++){
+            if(N%i == 0) 
+               return false;
+        }
+        return true;
+    }
